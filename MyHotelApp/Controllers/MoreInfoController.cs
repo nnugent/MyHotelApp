3c12231b1fde7cbd713be65c480bdb5c25605f2a
nnugent <nrnugent96@gmail.com>
2018-04-03 09:48:11 -0500

    Changed home page to relevant information, and fixed linking to new pages.

---
+++ MyHotelApp/Controllers/MoreInfoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyHotelApp.Controllers
+{
+    public class MoreInfoController : Controller
+    {
+        public ViewResult NotificationUpdates()
+        {
+            return View();
+        }
+        
+        public ViewResult EasyBilling()
+        {
+            return View();
+        }
+    }
+}
